9019e172cce374194379f01cc34f2a756e989f59
Delen0828 <shu343@gatech.edu>
2025-12-09 10:27:56 -0500

    Update redirect URL and remove header from CSV
    Changed the Prolific completion code in complete_study.php to use a new code. Removed the header row from taskflow.csv for consistency with data formatting.

--- complete_study.php
+++ complete_study.php
@@ -27,7 +27,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET' || $_SERVER['REQUEST_METHOD'] === 'POST
     http_response_code(200);
     echo json_encode([
         'success' => true,
-        'redirect_url' => 'https://app.prolific.com/submissions/complete?cc=CQGCCVCQ'
+        'redirect_url' => 'https://app.prolific.com/submissions/complete?cc=C5LN4A39'
     ]);
 } else {
     http_response_code(405);
